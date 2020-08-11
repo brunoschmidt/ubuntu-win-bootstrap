4ec9d478dbe88124c328dd8bf3564ff8ae5a36fb
Bruno Schmidt <bruno.schmidt@gmail.com>
2020-08-11 19:58:31 -0300

    feat: Color log output spliting installation sections

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,72 @@
 #!/usr/bin/env bash
-# these will run as the default non-privileged user.
+# These must run as the default non-privileged user.
 
-# create a temp dir
+# check sudo and password
+sudo id || exit
+
+# local functions
+function section {
+        echo -e "\e[33m\e[1m\n\n###\n### $*\e[0m"
+}
+function log {
+        echo -e "\e[33m\e[1m\n### $*\e[0m"
+}
+
+
+section "Create a temp dir"
 TEMPPATH="$(mktemp -d)"
-echo "Temporary storage : $TEMPPATH"
+log "Temporary storage : $TEMPPATH"
 
-# save distro
+section "Save distro"
 DISTRO=$(sed -rn '/CODENAME/ s/.*=//p' /etc/lsb-release)
-echo "Identified Distro : $DISTRO"
+log "Identified Distro : $DISTRO"
 
-# add pre-install requirements
-sudo apt install -y gnupg
+section "Add pre-install requirements"
+sudo apt install -y gnupg build-essential
 
-# switch apt to https mirror
+section "Fix nanosleep for WSL, restoring the old behaviour of nanosleep() to use CLOCK_MONOTONIC"
+cat > $TEMPPATH/nanosleep.c << EOL
+#define  _GNU_SOURCE
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/epoll.h>
+#include <dlfcn.h>
+int nanosleep(const struct timespec *req, struct timespec *rem) {
+    return clock_nanosleep(CLOCK_MONOTONIC, 0, req, rem);
+}
+int usleep(useconds_t usec) {
+    struct timespec req = { .tv_sec     = (usec / 1000000), .tv_nsec    = (usec % 1000000) * 1000, };
+    return nanosleep(&req, NULL);
+}
+static int min(int a, int b) { return a < b ? a : b; }
+static int (*waitfn)(int fd, struct epoll_event *events, int maxevents, int timeout, const __sigset_t *ss) = 0;
+int epoll_pwait(int fd, struct epoll_event *events, int maxevents, int timeout, const __sigset_t *ss) {
+  if (!waitfn)
+    waitfn = (int (*)(int fd, struct epoll_event *events, int maxevents, int timeout, const __sigset_t *ss)) dlsym(RTLD_NEXT, "epoll_pwait");
+  while (1) {
+    int r = waitfn(fd, events, maxevents, 0, ss);
+    if (r > 0 || timeout == 0)
+      return r;
+    if (timeout < 0)
+      usleep(100 * 10000);
+    else {
+      int s = min(1000, timeout);
+      timeout -= s;
+      usleep(s * 1000);
+    }
+  }
+  return -1;
+}
+EOL
+gcc -shared -fPIC -o "$TEMPPATH/libnanosleep.so" "$TEMPPATH/nanosleep.c"
+sudo mv "$TEMPPATH/libnanosleep.so" /usr/local/lib/libnanosleep.so
+echo /usr/local/lib/libnanosleep.so | sudo tee -a /etc/ld.so.preload > /dev/null
+
+section "Switch apt to https mirror"
 sudo sed -i '/^deb/ s,http://archive.ubuntu.com/ubuntu/,https://mirrors.edge.kernel.org/ubuntu/,' /etc/apt/sources.list
 
-# include repos
+section "Include repos"
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 echo "deb https://deb.nodesource.com/node_12.x $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list > /dev/null
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
@@ -24,38 +75,40 @@ echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu/ $DISTRO main" | sudo tee
 curl -s https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -o $TEMPPATH/packages-microsoft-prod.deb
 sudo dpkg -i $TEMPPATH/packages-microsoft-prod.deb
 
-# ensure we have the latest packages
+section "Ensure we have the latest packages"
 sudo apt update
 sudo apt -y full-upgrade
 
-# make sure we have the required libraries and tools already installed before starting.
+section "Make sure we have the required libraries and tools already installed before starting."
 sudo apt install -y \
   lsb-release build-essential curl wget gettext \
-  nmap ncat \
+  nmap ncat mosh \
   vim git \
   nodejs rustc dotnet-sdk-3.1 aspnetcore-runtime-3.1 dotnet-runtime-3.1\
-  jq \
+  jq unzip mmv \
   fish
 
-# install cli tools from rust
+section "Install cli tools from rust"
 cargo install hyperfine sd hx exa bat ripgrep fd-find
 
-# install winbind and support lib to ping WINS hosts
+section "Install winbind and support lib to ping WINS hosts"
 sudo apt install -y winbind libnss-winbind
 # need to append to the /etc/nsswitch.conf file to enable if not already done ...
 if ! grep -qc 'wins' /etc/nsswitch.conf ; then
   sudo sed -i '/hosts:/ s/$/ wins/' /etc/nsswitch.conf
 fi
 
-# basic .npmrc
+section "Basic .npmrc"
 echo prefix=$HOME/.local/ >> ~/.npmrc
 npm install -g npm
 
-# basic .gitconfig
+section "Basic .gitconfig"
 cat > ~/.gitconfig << EOL
 [user]
         name = Bruno Schmidt
         email = bruno.schmidt@gmail.com
+[core]
+        checkStat = minimal
 [alias]
         st = status
         co = checkout
@@ -64,20 +117,20 @@ cat > ~/.gitconfig << EOL
         cl = clone
         lol = log --graph --decorate --pretty=oneline
 	lola = log --graph --decorate --pretty=oneline --abbrev-commit --all
-        ammend = commit --amend
+        amend = commit --amend
 [push]
         default = simple
 [credential]
-        helper = cache --timeout 3600
+        helper = store
 [color]
-    ui = True
+        ui = True
 [status]
-    showUntrackedFiles = all
+        showUntrackedFiles = all
 [url "https://github.com/"]
-    insteadOf = gh
+        insteadOf = gh
 EOL
 
-# basic .bash_aliases
+section "Basic .bash_aliases"
 cat > ~/.bash_aliases << EOL
 #alias ls="ls -vh --color=auto"
 alias ls="exa -gbH"
@@ -87,8 +140,15 @@ alias g="git"
 alias map="xargs -n1"
 EOL
 
-# basic fish aliases
+section "Basic fish config"
 mkdir -p ~/.config/fish/conf.d/
+cat > ~/.config/fish/conf.d/welcome.fish << EOL
+function fish_greeting
+        update-motd --show-only
+end
+EOL
+
+section "Basic fish aliases"
 cat > ~/.config/fish/conf.d/alias.fish << EOL
 #alias ls "ls -vh --color=auto"
 alias ls "exa -gbH"
@@ -98,28 +158,31 @@ alias g "git"
 alias map "xargs -n1"
 EOL
 
-# fish load profile
+section "Fish load profile"
 cat > ~/.config/fish/conf.d/profile.fish << EOL
 status --is-login; and fenv ~/.profile
 EOL
 
-# profile PATH to cargo bins
+section "Profile PATH to cargo bins"
 cat >> ~/.profile << EOL
-# set PATH so it includes user's private Cargo bin if it exists
+# Set PATH so it includes user's private Cargo bin if it exists
 if [ -d "$HOME/.cargo/bin" ] ; then
     PATH="$HOME/.cargo/bin:$PATH"
 fi
 EOL
 
-# install omf
-curl -L https://get.oh-my.fish | fish
+section "Install omf"
+curl -L https://get.oh-my.fish > "$TEMPPATH/installer.fish"
+chmod +x "$TEMPPATH/installer.fish"
+"$TEMPPATH/installer.fish" --noninteractive
 fish -c "omf install agnoster"
 
-# install fish helpers
+section "Install fish helpers"
 fish -c "omf install foreign-env bass"
 
-# Enable UNC paths at cmd.exe to allow access to \\$wsl\
+section "Enable UNC paths at cmd.exe to allow access to \\$wsl\"
 which reg.exe >/dev/null && reg.exe add "HKCU\Software\Microsoft\Command Processor" /v DisableUNCCheck /t REG_DWORD /d 0x1 /f
 
-echo
-echo "You now need to close and restart the Bash shell"
+
+section "You now need to close and restart the Bash shell"
+rm -rf "$TEMPPATH"
